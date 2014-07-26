d141b29636d107ac06a93ddb15fe2835fce34561
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-26 12:05:52 +0100

    Remove pause in Build.bat

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,3 +1,2 @@
 Call ..\.nuget\nuget.exe restore ..\Our.Umbraco.PropertyValueConverters.sln -source "https://www.nuget.org/api/v2;https://www.myget.org/F/umbraco7nightly/"
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
-pause

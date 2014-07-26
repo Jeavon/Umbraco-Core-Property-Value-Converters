7b2358dfc1d75d34ead18753ac762b5d6933e6ce
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-26 11:53:49 +0100

    Adding MyGet source to restore command

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,4 +1,3 @@
-Call ..\.nuget\nuget.exe sources add -Name Umb7Nightly -Source https://www.myget.org/F/umbraco7nightly/ -NonInteractive
-Call ..\.nuget\nuget.exe restore ..\Our.Umbraco.PropertyValueConverters.sln
+Call ..\.nuget\nuget.exe restore ..\Our.Umbraco.PropertyValueConverters.sln -source "https://www.nuget.org/api/v2;https://www.myget.org/F/umbraco7nightly/"
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
 pause

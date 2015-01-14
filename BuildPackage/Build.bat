00b0497c89aec8c35ac5cda7ca3aab1798af03e5
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-01-14 11:51:33 +0000

    AppVeyor it all

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,2 +1,6 @@
-Call nuget.exe restore ..\Our.Umbraco.PropertyValueConverters.sln
-Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
+ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
+ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
+ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
+cd ..\BuildPackage\
+Call Tools\nuget.exe restore  ..\Our.Umbraco.PropertyValueConverters.sln
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" Package.build.xml

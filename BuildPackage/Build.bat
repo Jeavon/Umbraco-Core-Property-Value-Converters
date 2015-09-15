e4055a69e236c5406b2c325ccdf20cf6a262bb07
Jeavon <jeavon@crumpled-dog.com>
2015-09-15 18:26:05 +0100

    Update to MSBuild v14

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -4,4 +4,4 @@ ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 cd ..\BuildPackage\
 Call Tools\nuget.exe restore ..\Our.Umbraco.PropertyValueConverters.sln
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
+Call "%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" package.proj

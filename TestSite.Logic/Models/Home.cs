430196b88ee04fc0f44c69211090ec7638f518fb
Jeavon <jeavon@crumpled-dog.com>
2015-10-02 19:03:40 +0100

    Added some more tests using Ditto for mapping, all good!

--- TestSite.Logic/Models/Home.cs
+++ TestSite.Logic/Models/Home.cs
@@ -1,9 +1,20 @@
 ﻿namespace TestSite.Logic.Models
 {
-    using Our.Umbraco.PropertyConverters.Models;
+    using System.Collections.Generic;
 
+    using Umbraco.Core.Models;
+
+    using Our.Umbraco.PropertyConverters.Models;
+    
     public class Home
     {
+        public IPublishedContent ContentPicker { get; set; }
+        public IPublishedContent MainImage { get; set; }
+        public IEnumerable<IPublishedContent> FeaturedNews { get; set; }
+        public IEnumerable<IPublishedContent> FeaturedMedia { get; set; }
+        public IEnumerable<IPublishedContent> FeaturedMember { get; set; }
+        public IEnumerable<IPublishedContent> MultiMedia { get; set; }
+        public IPublishedContent MultiMediaSingle { get; set; }
         public RelatedLinks RelatedLinks { get; set; }
     }
 }
